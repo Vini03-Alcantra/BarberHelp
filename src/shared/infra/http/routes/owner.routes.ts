74c7db9d1687eef3be95d93f48397fb423715a7f
vinicius de alcântra Barros <viniciusalcantarabarros@gmail.com>
2022-01-28 16:34:13 -0300

    Read Owner

--- src/shared/infra/http/routes/owner.routes.ts
+++ src/shared/infra/http/routes/owner.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { CreateOwnerController } from "../../../../modules/owner/useCases/createOwner/CreateOwnerController";
 import { DeleteOwnerController } from "../../../../modules/owner/useCases/deleteOwner/DeleteOwnerController";
+import { ReadOwnerController } from "../../../../modules/owner/useCases/readOwner/ReadOwnerController";
 import { UpdateOwnerController } from "../../../../modules/owner/useCases/updateOwner/UpdateOwnerController";
 
 const ownerRouter = Router()
@@ -9,8 +10,10 @@ const ownerRouter = Router()
 const createOwnerController = new CreateOwnerController()
 const deleteOwnerController = new DeleteOwnerController()
 const updateOwnerController = new UpdateOwnerController()
+const readOwnerController = new ReadOwnerController()
 
 ownerRouter.post("/", createOwnerController.handle)
 ownerRouter.delete("/", deleteOwnerController.handle)
 ownerRouter.patch("/", updateOwnerController.handle)
+ownerRouter.get("/", readOwnerController.handle)
 export { ownerRouter }
