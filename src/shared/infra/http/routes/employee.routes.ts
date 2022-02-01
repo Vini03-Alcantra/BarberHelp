5fc1d842a93f8c56f4ee5729644422ce64d63bc4
vinicius de alcântra Barros <viniciusalcantarabarros@gmail.com>
2022-02-01 10:34:20 -0300

    Finalizando estrutura de routes employee

--- src/shared/infra/http/routes/employee.routes.ts
+++ src/shared/infra/http/routes/employee.routes.ts
@@ -1,5 +1,21 @@
 import { Router } from "express";
+import { CreateEmployeeController } from "../../../../modules/employee/useCases/CreateEmployee/CreateEmployeeController";
+import { DeleteEmployeeController } from "../../../../modules/employee/useCases/DeleteEmployee/DeleteEmployeeControlller";
+import { ReadEmployeeController } from "../../../../modules/employee/useCases/ReadEmployee/ReadEmployeeController";
+import { UpdateEmployeeController } from "../../../../modules/employee/useCases/UpdateEmployee/UpdateEmployeeController";
 
 const employeeRouter = Router()
+
+const createEmployeeController = new CreateEmployeeController()
+const deleteEmployeeController = new DeleteEmployeeController()
+const readEmployeeController = new ReadEmployeeController()
+const updateEmployeeController = new UpdateEmployeeController()
+
+employeeRouter.post("/", createEmployeeController.handle)
+employeeRouter.delete("/", deleteEmployeeController.handle)
+employeeRouter.get("/", readEmployeeController.handle)
+employeeRouter.patch("/", updateEmployeeController.handle)
+
+
 
 export { employeeRouter }
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -4,6 +4,7 @@ import { ownerRouter } from "./owner.routes";
 import { addressRouter } from "./address.routes";
 import { clientRouter } from "./client.routes";
 import { establishmentRouter } from "./establishment.routes";
+import { employeeRouter } from "./employee.routes";
 
 
 const router = Router()
@@ -13,5 +14,6 @@ router.use("/owner", ownerRouter)
 router.use("/address", addressRouter)
 router.use("/client", clientRouter)
 router.use("/establishment", establishmentRouter)
+router.use("/employee", employeeRouter)
 
 export { router }
