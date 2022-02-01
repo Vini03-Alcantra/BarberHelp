d897c5518a9cd035ce2dbfcd761077777e8c5aad
vinicius de alcântra Barros <viniciusalcantarabarros@gmail.com>
2022-02-01 13:02:16 -0300

    AddressClient

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -6,6 +6,7 @@ import { clientRouter } from "./client.routes";
 import { establishmentRouter } from "./establishment.routes";
 import { employeeRouter } from "./employee.routes";
 import { servicesRouter } from "./services.routes";
+import { addressClientRouter } from "./address.client.routes";
 
 
 const router = Router()
@@ -17,5 +18,6 @@ router.use("/client", clientRouter)
 router.use("/establishment", establishmentRouter)
 router.use("/employee", employeeRouter)
 router.use("/services", servicesRouter)
+router.use("/addressClient", addressClientRouter)
 
 export { router }
