9cd827317466ff76279b6d809a1eb794a8f86f0e
Vinícius Alcântra <viniciusalcantarabarros@gmail.com>
2022-02-18 14:09:11 -0300

    alterações

---
+++ prisma/data_fakes/DataFakeSeeds.ts
@@ -0,0 +1,39 @@
+const owner = {
+    nome: "Tyrion Lannister",
+    cpf: "83759543204",
+    birthday: "12/03/1978",
+    email: "tyrion@got.com",
+    phoneNumber: "859872364568",
+    password: "12345678",
+}
+
+const address = {
+    street: "Rua Witerfell",
+    number_address: 12,
+    complement: "Ao note de Westeros",
+    neighborhood: "Stark Te",
+    city: "Witerfell",
+    state: "Westeros",
+    cep: "12345676512",
+    reference_point: "Perto do Muro do norte"
+}
+
+const addressClient = {
+    street: "Casa Dornelli",
+    number_address: 45,
+    complement: "Pertyo do palácio",
+    neighborhood: "Dor",
+    city: "Dom",
+    reference_point: "atravessando o oceano",
+    state: "Dornel",
+    cep: "1234763"
+}
+
+
+
+
+export const DataFakeSeeds = {
+    owner,
+    address,
+    addressClient
+}
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,16 +1,45 @@
 import {PrismaClient} from "@prisma/client"
+import { DataFakeSeeds } from "./data_fakes/DataFakeSeeds";
 
 const prisma = new PrismaClient();
+
+const {owner, address, addressClient} = DataFakeSeeds;
+
+
 
 async function main(){
     await prisma.owner.create({
         data: {
-            nome: "Robb Stark",
-            email: "robbstark@got.com",
-            phoneNumber: "85988124209",
-            cpf: "19812349609",
-            birthday: "20/09/1987",
-            password: "12345678"
+            nome: owner.nome,
+            email: owner.email,
+            phoneNumber: owner.phoneNumber,
+            cpf: owner.cpf,
+            birthday: owner.birthday,
+            password: owner.password
+        }
+    }),
+    await prisma.address.create({
+        data: {
+            street: address.street,
+            number_address: address.number_address,
+            complement: address.complement,
+            neighborhood: address.neighborhood,
+            city: address.city,
+            state: address.state,
+            cep: address.cep,
+            reference_point: address.reference_point 
+        }
+    }),
+    await prisma.addressClient.create({
+        data: {
+            street: addressClient.street,
+            number_address: addressClient.number_address,
+            complement: addressClient.complement,
+            neighborhood: addressClient.neighborhood,
+            city: addressClient.city,
+            reference_point: addressClient.city,
+            state: addressClient.state,
+            cep: addressClient.cep
         }
     })
 }
