4e07eea3ee3729138c3e382f0879143e49714e5b
Vinícius Alcântra <viniciusalcantarabarros@gmail.com>
2022-02-19 14:36:16 -0300

    JWT authenticate

--- src/midlewares/error-handle.midleware.ts
+++ src/midlewares/error-handle.midleware.ts
@@ -1,10 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import status from "http-status"
+import { DatabaseError } from "./modelsMiddlewares/errors/database.error.model";
+import { ForbiddenError } from "./modelsMiddlewares/errors/forbidden.error.model";
 
 function errorHandler(error: any, req: Request,res: Response, next: NextFunction){
-    if (error) {
+    if (error instanceof DatabaseError) {
         res.sendStatus(status.BAD_REQUEST)
-    } else if(error){
+    } else if(error instanceof ForbiddenError){
         res.sendStatus(status.FORBIDDEN)
     } else {
         res.sendStatus(status.INTERNAL_SERVER_ERROR)
